262984e8aa17bf9f3f28a0675fc269b374d7d6a2
Spas Angelov <spasangelov@gmail.com>
2020-05-11 20:20:52 -0600

    Week 11/5 monday

--- Computational Fluid Mechanics/src/main.cpp
+++ Computational Fluid Mechanics/src/main.cpp
@@ -1,38 +1,52 @@
 #include <iostream>
+#include <array>
+#include <fstream>
+#include <math.h>
+
+#define ITERATION_COUNT 400
 
 
 
-double rungeKutta45(double startVal, double startTime, double (*function)(double, double), double h, int itterations);
+void rungeKutta4(double w[], double t[], double (*function)(double, double), double h, int itterations);
 double testFunction(double time, double val);
 
 int main() {
-	std::cout << "Hello World" << std::endl;
-	std::cout << "Basic Test Function: " << rungeKutta45(.5, 0, testFunction, .5, 4) << std::endl;
+
+	double y[ITERATION_COUNT];
+	double t[ITERATION_COUNT];
+
+	t[0] = 0;
+	y[0] = .5;
+
+	rungeKutta4(y, t, testFunction, .01, ITERATION_COUNT);
+
+
+	std::ofstream fileManager("dataForPlot.dat");
+	if (!fileManager.fail()) {
+		for (int i = 0; i < ITERATION_COUNT; i++)
+			fileManager << t[i] << " " << y[i] << std::endl;
+	}
+	else
+		std::cout << "Warning File Failed to Create" << std::endl;
+
 
 }
 
-
-double rungeKutta45(double startVal, double startTime, double (*function)(double, double), double h, int itterations){
-	double w = startVal;
+//To Do: Add Functionality for systems of differential equations
+void rungeKutta4(double w[], double t[], double (*function)(double, double), double h, int itterations){
 	double k1, k2, k3, k4;
-	double t = startTime;
 
-
-
-	for (int i = 0; i < itterations; i++) {
-		k1 = h * function(t, w);
-		k2 = h * function(t + h / 2, w + k1 / 2);
-		k3 = h * function(t + h / 2, w + k2 / 2);
-		k4 = h * function(t + h, w + k3);
-		w = w + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
-		t += h;
+	for (int i = 0; i < itterations-1; i++) {
+		k1 = h * function(t[i], w[i]);
+		k2 = h * function(t[i] + h / 2, w[i] + k1 / 2);
+		k3 = h * function(t[i] + h / 2, w[i] + k2 / 2);
+		k4 = h * function(t[i] + h, w[i] + k3);
+		w[i+1] = w[i] + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+		t[i+1] = t[i] + h;
 	}
 
-
-	return w;
 }
 double testFunction(double time, double val) {
-	return val - time * time + 1;
+	return  sin(val) * time;
 }
-
 
